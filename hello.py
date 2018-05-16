53388f6d1f70e628c01ce562beec407195958165
Curt J. Sampson <cjs@cynic.net>
2018-05-16 20:59:16 +0900

    hello.py: Add unit test

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello, world.")
+def hello(name):
+    return 'Hello, {}.'.format(name)
+
+def test_hello():
+    assert 'Hello, Fred.' == hello('Fred')
+
+if __name__ == '__main__':
+    print(hello('Taro'))
